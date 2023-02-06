202f9f36e43d9d17b1686eb22f36cd4166f7bfb2
DanielRiddersporre <daniel.riddersporre@icloud.comgit config --global user.name DanielRiddersporregit config --global user.email daniel.riddersporre@icloud.com>
2023-02-06 18:35:58 +0100

    added the projects for both ui and api

---
+++ steam_achievement_tracker_ui/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <span>Number of achievements collected during 2023</span>
+      </header>
+    </div>
+  );
+}
+
+export default App;
